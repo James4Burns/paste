d83259188749fc9ec002f8b899fe4be5c596950a
James Burns <James4Burns@users.noreply.github.com>
2022-01-21 07:03:16 -0500

    Add title, description, and viewport tags to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,30 @@
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import Layout from "components/layout";
 import { defaultSettings } from "library/settings";
 import "styles/globals.css";
 import type { TPageComponent } from "types/component";
 
-const App: NextPage<AppProps<TPageComponent>> = ({ Component }) => {
+const MyApp: NextPage<AppProps<TPageComponent>> = ({ Component }) => {
   const settings = defaultSettings;
 
   return (
-    <Layout theme={settings.theme} title={settings.title}>
-      <Component settings={settings} />
-    </Layout>
+    <>
+      <Head>
+        <title>Paste</title>
+        <meta
+          name="description"
+          content="An encrypted pastebin made in Next.js."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Layout theme={settings.theme} title={settings.title}>
+        <Component settings={settings} />
+      </Layout>
+    </>
   );
 };
 
-export default App;
+export default MyApp;
